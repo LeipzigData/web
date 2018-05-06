f4a7d9830e1fa8d208033b35ce6ddbde405cd244
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2018-05-06 21:30:34 +0200

    Anzeige Zukunftsdiplom gefixt

--- info/php/Zukunftsdiplom.php
+++ info/php/Zukunftsdiplom.php
@@ -10,58 +10,71 @@ function pass() {
   $out='<h2> Die Module </h2>';
   $out.=dieModule();
 
+  $out='<h2> Die Veranstaltungen </h2>';
+  $out.=dieVeranstaltungen();
+
   $out.='<h2> Die Partner </h2>';
   $out.=diePartner();
   return $out;
 }
 
-function diePartnerOld() {
+function dieModule() {
   $query = '
 PREFIX ld: <http://leipzig-data.de/Data/Model/> 
 PREFIX nl: <http://nachhaltiges-leipzig.de/Data/Model#> 
-PREFIX ical: <http://www.w3.org/2002/12/cal/ical#>
-construct { ?a ?p ?q . } 
-from <http://leipzig-data.de/Data/Zukunftspass/>
+select  ?a ?l ?ziele
+from <http://leipzig-data.de/Data/Zukunftsdiplom/>
 Where { 
-?a  a nl:Partner ; ?p ?q . 
+  ?a a nl:Modul ; rdfs:label ?l; ld:Lernziele ?ziele . 
 }
 ';
   
   $sparql = new EasyRdf_Sparql_Client('http://leipzig-data.de:8890/sparql');
-  $result=$sparql->query($query); // CONSTRUCT funktioniert nicht mit php-7
+  $result=$sparql->query($query); 
   // echo $result->dump("turtle");
   $s=array();
-  foreach ($result->allOfType("nl:Partner") as $v) {
-      $s[$v->getUri()]=displayPartner($v);
+  foreach ($result as $row) {
+      $s[]='<tr><td>'.$row->l.'</td><td>'.$row->ziele.'</td></tr>';
   }
-  return join($s,"\n") ; 		
+  return '<table align="center">
+<tr><th>Modul</th><th>Lernziele</th></tr>
+'.join($s,"\n").'
+</table>' ; 		
 }
 
 function diePartner() {
-    $graph=new EasyRdf_Graph();
-    $graph->parsefile("http://www.leipzig-data.de/RDFData/Zukunftsdiplom.ttl");
+  $query = '
+PREFIX ld: <http://leipzig-data.de/Data/Model/> 
+PREFIX nl: <http://nachhaltiges-leipzig.de/Data/Model#> 
+PREFIX ical: <http://www.w3.org/2002/12/cal/ical#>
+select * 
+from <http://leipzig-data.de/Data/Zukunftsdiplom/>
+Where { 
+?a  a nl:Partner ; rdfs:label ?l . 
+optional { ?a nl:Teilnahme ?tn . }
+optional { ?a ld:contact ?contact . }
+optional { ?a ld:hasAddress ?address . }
+optional { ?a foaf:homepage ?url . }
+}
+';
+  
+  $sparql = new EasyRdf_Sparql_Client('http://leipzig-data.de:8890/sparql');
+  $result=$sparql->query($query); 
   // echo $result->dump("turtle");
   $s=array();
-  foreach ($graph->allOfType("nl:Partner") as $v) {
-      $s[$v->getUri()]=displayPartner($v);
+  foreach ($result as $row) {
+      $s[]=displayPartner($row);
   }
-  sort($s);
   return join($s,"\n") ; 		
 }
 
 function displayPartner($v) {
-    $a=$v->getUri();
-    $label=$v->get('rdfs:label'); 
-    $contact=$v->get('ld:contact'); 
-    $gelistet=$v->get('nl:Ferienpass');
-    $teilnahme=$v->get('nl:Teilnahme');
-    $abschluss=$v->get('nl:Abschlussveranstaltung');
-    $com=$v->get('rdfs:comment');
-    if (strpos($teilnahme,"bisher keine Antwort")!==false) { return ''; }
+    if (isset($v->tn)) { return ''; }
     $out='
-<h3> <a href="getdata.php?show='.$a.'">'.$label.'</a></h3>
-<div class="row">Ferienpass: '.$gelistet.' </div>
-<div class="row">Anmerkungen: '.$teilnahme.' </div>';
+<h3> <a href="getdata.php?show='.$v->a.'">'.$v->l.'</a></h3>';
+    if (isset($v->contact))   { $out.='<div class="row">Kontakt: '.$v->contact.' </div>'; }
+    if (isset($v->address))   { $out.='<div class="row">Adresse: '.$v->address.' </div>'; }
+    if (isset($v->url))   { $out.='<div class="row">URL: <a href='.$v->url.'>'.$v->url.'</a> </div>'; }
     return $out;
 }
 
@@ -73,38 +86,50 @@ function mehrzeilig($a) {
     return "<ul>".join("\n",$b)."</ul>";
 }
 
-function dieModule() {
-    $graph=new EasyRdf_Graph();
-    $graph->parsefile("http://www.leipzig-data.de/RDFData/Zukunftsdiplom.ttl");
+function dieVeranstaltungen() {
+  $query = '
+PREFIX ld: <http://leipzig-data.de/Data/Model/> 
+PREFIX nl: <http://nachhaltiges-leipzig.de/Data/Model#> 
+PREFIX ical: <http://www.w3.org/2002/12/cal/ical#>
+select * 
+from <http://leipzig-data.de/Data/Zukunftsdiplom/>
+Where { 
+?a  a nl:Event ; rdfs:label ?l . 
+optional { ?a ical:description ?desc . }
+optional { ?a ld:Altersgruppe ?ag . }
+optional { ?a ld:Veranstaltungsmodus ?wie . }
+optional { ?a ld:Veranstaltungsort ?wo . }
+optional { ?a ld:Veranstalter ?wer . }
+optional { ?a ld:Kosten ?ko . }
+optional { ?a ld:Anmerkung ?an . }
+optional { ?a ical:url ?url . }
+}
+';
+  
+  $sparql = new EasyRdf_Sparql_Client('http://leipzig-data.de:8890/sparql');
+  $result=$sparql->query($query); 
   // echo $result->dump("turtle");
   $s=array();
-  foreach ($graph->allOfType("nl:Modul") as $v) {
-      $s[$v->getUri()]=displayEvent($v);
+  foreach ($result as $row) {
+      $s[]=displayEvent($row);
   }
-  sort($s);
+  //sort($s);
   return join($s,"\n") ; 		
 }
 
 function displayEvent($v) {
-    $a=$v->getUri();
-    $label=$v->get('rdfs:label');
-    $description=$v->get('ical:description');
-    $altersgruppe=$v->get('ld:Altersgruppe');
-    $kosten=$v->get('ld:Kosten');
-    $modus=$v->get('ld:Veranstaltungsmodus');
-    $ort=$v->get('ld:Veranstaltungsort');
-    $wer=$v->get('ld:Veranstalter');
-    $url=$v->get('ical:url');
+    $description=$v->desc;
     $out='
-<h3> <a href="getdata.php?show='.$a.'">'.$label.'</a></h3>
+<h3> <a href="getdata.php?show='.$v->a.'">'.$v->l.'</a></h3>
 <div class="row">
 <dl><dd> <strong>Beschreibung:</strong> '.str_replace("\n",'<br/>',$description).' </dd>
-<dd> <strong>Wie angeboten:</strong> '.$modus.'</dd>
-<dd> <strong>Wo angeboten:</strong> '.$ort.'</dd>
-<dd> <strong>Altersgruppe:</strong> '.$altersgruppe.'</dd>
-<dd> <strong>Kosten:</strong> '.$kosten.'</dd>
-<dd> <strong>URL:</strong> '.$url.'</dd>
-<dd> <strong>Veranstalter:</strong> '.$wer.'</dd></dl></div>
+<dd> <strong>Wie angeboten:</strong> '.$v->wie.'</dd>
+<dd> <strong>Wo angeboten:</strong> '.$v->wo.'</dd>
+<dd> <strong>Altersgruppe:</strong> '.$v->ag.'</dd>
+<dd> <strong>Kosten:</strong> '.$v->ko.'</dd>
+<dd> <strong>Anmerkungen:</strong> '.$v->an.'</dd>
+<dd> <strong>URL:</strong> '.$v->url.'</dd>
+<dd> <strong>Veranstalter:</strong> '.$v->wer.'</dd></dl></div>
 ';
     return $out;
 }
--- info/zukunftsdiplom.php
+++ info/zukunftsdiplom.php
@@ -10,11 +10,11 @@ include_once("php/Zukunftsdiplom.php");
 $content='      
 <div class="container"> 
 
-<h2 align="center">Aktueller Stand zum Angebotsaufbau für das Leipziger
+<h2 align="center">Aktueller Stand zum Angebotsaufbau für das <br/>  Leipziger
 Zukunftsdiplom im Sommer 2018</h2>
 
-<div class="row"> Die folgende Übersicht enthält die bisher eingegangenen
-Meldungen von Modulangeboten für das Leipziger Zukunftsdiplom, das in den
+<div class="row"> Die folgende Übersicht enthält die Module und
+Veranstaltungsangebote für das Leipziger Zukunftsdiplom, das in den
 Sommerferien 2018 in enger Abstimmung mit dem Ferienpassprogramm der Stadt
 Leipzig erstmals vergeben wird. </div>
 
