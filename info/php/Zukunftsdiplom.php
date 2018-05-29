5616067c7c4c1c498abb97f2f77b3ec019cf4b8a
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2018-05-29 09:36:55 +0200

    xx

--- info/php/Zukunftsdiplom.php
+++ info/php/Zukunftsdiplom.php
@@ -2,6 +2,8 @@
 
 require_once("lib/EasyRdf.php");
 
+// --- Die main Funktion
+
 function pass() {  
   EasyRdf_Namespace::set('ical', 'http://www.w3.org/2002/12/cal/ical#');
   EasyRdf_Namespace::set('ld', 'http://leipzig-data.de/Data/Model/');
@@ -18,8 +20,13 @@ function pass() {
 
   $out.='<h2> Die Partner </h2>';
   $out.=diePartner();
+  
+  $out.='<h2> Die Orte </h2>';
+  $out.=dieOrte();
   return $out;
 }
+
+// --- Hilfsfunktionen
 
 function listeVeranstaltungen($praedikat,$objekt) {
   $query = '
@@ -45,6 +52,49 @@ function zdtrim($s) {
     return str_replace("http://leipzig-data.de/Data/",'',$s);
 }
 
+function mehrzeilig($a) {
+    $b=array(); 
+    foreach($a as $event) {
+        $b[]='<li> '.str_replace("\n",'<br/>',$event).' </li>' ;
+    }
+    return "<ul>".join("\n",$b)."</ul>";
+}
+
+// --- Die Hauptmodule
+
+function dieOrte() {
+  $query = 'PREFIX ld: <http://leipzig-data.de/Data/Model/> 
+PREFIX nl: <http://nachhaltiges-leipzig.de/Data/Model#> 
+PREFIX ical: <http://www.w3.org/2002/12/cal/ical#>
+select distinct(?wo) ?l ?adr ?e ?url
+from <http://leipzig-data.de/Data/Zukunftsdiplom/>
+Where {
+?a ld:Veranstaltungsort ?wo . ?wo rdfs:label ?l .
+optional { ?wo ld:hasAddress ?adr . }
+optional { ?wo ld:erreichbar ?e . }
+optional { ?wo foaf:homepage ?url . }
+} ';
+  
+  $sparql = new EasyRdf_Sparql_Client('http://leipzig-data.de:8890/sparql');
+  $result=$sparql->query($query); 
+  $s=array();
+  foreach ($result as $row) {
+      $s[]=displayOrt($row);
+  }
+  return join($s,"\n") ; 		
+}
+
+function displayOrt($v) {
+    $out='
+<h3> <a href="getdata.php?show='.$v->a.'">'.$v->l.'</a></h3>
+<div class="row"> <dl>';
+    if (isset($v->adr))   { $out.='<dd> <strong>Adresse:</strong> '.$v->adr.' </dd>'; }
+    if (isset($v->e))   { $out.='<dd> <strong>Erreichbarkeit:</strong> '.$v->wie.'</dd>'; }
+    if (isset($v->url))   { $out.='<dd> <strong>URL:</strong> '.$v->url.'</dd>'; }
+    $out.='</dl></div>';
+    return $out;
+}
+
 function dieModule() {
   $query = '
 PREFIX ld: <http://leipzig-data.de/Data/Model/> 
@@ -62,7 +112,7 @@ Where {
   $s=array();
   foreach ($result as $row) {
       $u=listeVeranstaltungen("ld:Einordnung","<$row->a>"); 
-      $s[]='<tr><td>'.$row->a.'</td><td>'.$row->l.'</td><td>'.$row->ziele.
+      $s[]='<tr><td>'.zdtrim($row->a).'</td><td>'.$row->l.'</td><td>'.$row->ziele.
           '</td><td>'.$u.'</td></tr>';
   }
   return '<table align="center" border="3" width="70%">
@@ -88,7 +138,7 @@ Where {
   $s=array();
   foreach ($result as $row) {
       $u=listeVeranstaltungen("ld:Einordnung","<$row->a>"); 
-      $s[]='<tr><td>'.$row->a.'</td><td>'.$row->l.'</td><td>'.$u.'</td></tr>';
+      $s[]='<tr><td>'.zdtrim($row->a).'</td><td>'.$row->l.'</td><td>'.$u.'</td></tr>';
   }
   return '<table align="center" border="3" width="70%">
 <tr><th>Themenbereich</th><th>Thema</th><th>Veranstaltungen</th></tr>
@@ -132,14 +182,6 @@ function displayPartner($v) {
     return $out;
 }
 
-function mehrzeilig($a) {
-    $b=array(); 
-    foreach($a as $event) {
-        $b[]='<li> '.str_replace("\n",'<br/>',$event).' </li>' ;
-    }
-    return "<ul>".join("\n",$b)."</ul>";
-}
-
 function dieVeranstaltungen() {
   $query = '
 PREFIX ld: <http://leipzig-data.de/Data/Model/> 
@@ -157,7 +199,7 @@ optional { ?a ld:Veranstalter ?wer . }
 optional { ?a ld:Kosten ?ko . }
 optional { ?a ld:Anmerkung ?an . }
 optional { ?a ical:url ?url . }
-}
+} order by ?a 
 ';
   
   $sparql = new EasyRdf_Sparql_Client('http://leipzig-data.de:8890/sparql');
@@ -171,23 +213,38 @@ optional { ?a ical:url ?url . }
   return join($s,"\n") ; 		
 }
 
+/* 
+
+Vorschlag Schirmer vom 24.5., was angegeben werden soll
+* Titel der Veranstaltung
+* Kurzbeschreibung 
+* Datum, Uhrzeit, Ort - Veranstaltungsmodus
+* ggf. Erreichbarkeit 
+* Kosten
+* Anmerkungen: Was ist mitzubringen oder zu beachten? (ganz kurz)
+Aus meiner Sicht weiter:
+* Veranstalter
+* Altersgruppe
+* URL der Veranstaltung
+
+
+*/
+
 function displayEvent($v) {
-    $description=$v->desc;
     $out='
-<h3> <a href="getdata.php?show='.$v->a.'">'.$v->l.'</a></h3>
-<div class="row">
-<dl><dd> <strong>Beschreibung:</strong> '.str_replace("\n",'<br/>',$description).' </dd>
-<dd> <strong>Wie angeboten:</strong> '.$v->wie.'</dd>
-<dd> <strong>Wo angeboten:</strong> '.$v->wo.'</dd>
-<dd> <strong>Altersgruppe:</strong> '.$v->ag.'</dd>
-<dd> <strong>Kosten:</strong> '.$v->ko.'</dd>
-<dd> <strong>Anmerkungen:</strong> '.$v->an.'</dd>
-<dd> <strong>URL:</strong> '.$v->url.'</dd>
-<dd> <strong>Veranstalter:</strong> '.$v->wer.'</dd></dl></div>
-';
+<h3> '.zdtrim($v->a).': <a href="getdata.php?show='.$v->a.'">'.$v->l.'</a></h3>
+<div class="row"> <dl>';
+    if (isset($v->desc))   { $out.='<dd> <strong>Beschreibung:</strong> '.str_replace("\n",'<br/>',$v->desc).' </dd>'; }
+    if (isset($v->wie))   { $out.='<dd> <strong>Veranstaltungsmodus:</strong> '.$v->wie.'</dd>'; }
+    if (isset($v->wo))   { $out.='<dd> <strong>Veranstaltungsort:</strong> '.$v->wo.'</dd>'; }
+    if (isset($v->ag))   { $out.='<dd> <strong>Altersgruppe:</strong> '.$v->ag.'</dd>'; }
+    if (isset($v->ko))   { $out.='<dd> <strong>Kosten:</strong> '.$v->ko.'</dd>'; }
+    if (isset($v->an))   { $out.='<dd> <strong>Anmerkungen:</strong> '.$v->an.'</dd>'; }
+    if (isset($v->wer))   { $out.='<dd> <strong>Veranstalter:</strong> '.$v->wer.'</dd>'; }
+    if (isset($v->url))   { $out.='<dd> <strong>URL:</strong> '.$v->url.'</dd>'; }
+    $out.='</dl></div>';
     return $out;
 }
-
-
+  
 // ---- test ----
 // echo pass();
