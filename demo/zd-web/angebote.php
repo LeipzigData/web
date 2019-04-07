8bb298f89178f75bb53193926b65c6dc833f4258
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2019-04-07 09:29:16 +0200

    small fix

--- demo/zd-web/angebote.php
+++ demo/zd-web/angebote.php
@@ -12,6 +12,10 @@ $content='
 <div class="container">
 <h2 align="center">Die Angebote</h2>
 
+<p>Die Datenbasis wird einmal täglich über einen Cron-Job aktualisiert,
+Änderungen in der Nachhaltigekitsdatenbank sind also nicht sofort hier
+sichtbar. </p>
+
 '.dieVeranstaltungen().'
 </div>
 ';
