aba0641ec78a302349288eae9f98e3fb808604a0
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2019-11-12 19:37:37 +0100

    GTA Tag ergänzt

---
+++ demo/zd-web/gta.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Hans-Gert Gräbe
+ * Date: 2019-02-24
+ * Last Update: 2019-11-12
+ */
+
+include_once("layout.php");
+include_once("Zukunftsdiplom.php");
+
+function getGTAServices() {
+    $src="Dumps/Services.json";
+    $string = file_get_contents($src);
+    $res = json_decode($string, true);
+    $s;
+    foreach($res as $row) {
+        if (($row["type"]=="Service") and ($row["service_type"]=="GTA")) {
+            $s[$row["id"]]=displayService($row);
+        }
+    }
+    return join("\n",$s);
+}
+
+$content='
+<div class="container">
+<h2 align="center">Die GTA-Angebote</h2>
+
+<p>Auf dieser Seite werden alle Einträge gelistet, die in die Kategorie
+"Services" uns den Service-Type "GTA" fallen. </p>
+
+'.getGTAServices().'
+</div>
+';
+echo showPage($content);
+
+?>
