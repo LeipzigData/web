ae4ee67f7478e42de0b86917cc2d4a58800fa59e
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2019-12-14 10:06:04 +0100

    Beitrag Schallehn DTS 11.11.2019

---
+++ demo/zd-web/zd.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * User: Hans-Gert Gräbe
+ * Date: 2018-06-07
+ * Last Update: 2019-11-12
+ */
+
+include_once("layout.php");
+
+$content='      
+<div class="container">
+<div class="row">
+<div  class="col-lg-3 col-sm-1"></div><div  class="col-lg-6 col-sm-10">
+<h3>Mach mit beim Zukunftsdiplom!</h3>
+
+<p><strong>Das Zukunftsdiplom 2019 ist abgeschlossen, die
+Abschlussveranstaltung mit der Würdigung der aktivsten Teilnehmer und der
+Übergabe der Zukunftsdiplome hat während der Verleihung des Leipziger
+Zukunftspreises im Rathaus am 26. September 2019 stattgefunden.</p>  
+
+<p>Eine weitere Runde des Zukunftsdiploms ist für 2020 fest
+eingeplant.</strong></p>
+
+<h3>Und so funktioniert das Zukunftsdiploms</h3>
+
+<p>Wo kommt deine Kleidung oder das Essen eigentlich her? Was hat unsere
+  Fortbewegung mit dem Klimawandel zu tun? Was tun die Pflanzen und Tiere im
+  Auwald eigentlich für uns? Und wie geht eine gerechte Welt im Kleinen?</p>
+
+<p>Diese und mehr Fragen kannst du mit den verschiedenen Angeboten
+  beantworten, die wir hier vorstellen.  Besuche mehrere davon, lass dir in
+  deinem Zukunftspass die Teilnahme bestätigen und gewinne spannende
+  Preise. </p>
+
+<p>Dein Zukunftsdiplom erhältst du, wenn du mindestens drei Angebote aus
+  verschiedenen Bereichen besucht hast und deinen Zukunftspass zur Auswertung
+  einreichst.</p>
+
+<p>Viel Spaß wünschen wir dir von der Zukunftsakademie Leipzig e.V.</p>
+
+Mehr zum Zukunftsdiplom findest du 
+<ul>
+<li> <a href="https://www.zukunftsakademie-leipzig.de/ziele/zukunftsdiplom/"
+>auf den Webseiten der Zukunftsakademie</a></li>
+<li> in einer <a href="http://leipzig-netz.de/index.php5/ZAK.Zukunftsdiplom"
+>Darstellung der Hintergründe</a> auf den Seiten des Leipzig-Wikis</li>
+</ul>
+
+</div> </div>
+
+';
+echo showPage($content);
+
+?>
