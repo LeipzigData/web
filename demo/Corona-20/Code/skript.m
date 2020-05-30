f7b0971c946858a8993658e5c62c35531eee827f
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2020-05-30 22:02:30 +0200

    update

--- demo/Corona-20/Code/skript.m
+++ demo/Corona-20/Code/skript.m
@@ -1,14 +1,19 @@
 batchload("BasicData-Maxima.txt");
 /* Mit der Datenfülle ist das lsquares mittlerweile überfordert */
 load("lsquares");
+/* Gauss- und Fehlerfunktion (Integral der Gaussverteilung) */
 f(t):=exp(c-((t-m)/s)^2);  /* Gauss distribution */
 g(t):=c-((t-m)/s)^2;       /* log of Gauss distribution */
 h(t):=0.8862*exp(c)*s*(erf((t-m)/s)+erf(m/s)); /* Error function */
 
+/* Logistische Funktion und deren Ableitung */
+l(t):=K/(1+exp(c-r*t));
+define(dl(t),diff(l(t),t));
+
 /* Extract increment list \Delta(S)=[s_i-s_{i-1},i=1..len(S)] from a list */
 Delta(l):=l-append([0],reverse(rest(reverse(l))))$
 
-/* Collect data for a given country into a single nested list
+/* Collect data for a given country into a single nested list. 
 infected means positively tested
 */
 getData(Land):=block([u,v,T,I,R,D,RD],
@@ -28,44 +33,36 @@ getData(Land):=block([u,v,T,I,R,D,RD],
 
 /* == Fitting procedure == */
 	 
-FittingDelta(G,tr):=block([G1,G2,G3,G4,G5],
-  /* tr is a lower threshold for data to be counted
-     from the list of increments */ 
-  /* Compute the Deltas */
-  G1:map("[",map(first,G),Delta(map(second,G))),
-  /* Filter out small entries */
-  G2:sublist(G1,lambda([u],second(u)>tr)),
-  /* Compute the log data */
-  G3:map(lambda([u],[first(u),float(log(second(u)))]),G2),
+FitData(G,tr):=block([G1,G2,G3],
+  /* G is already a Delta without zero values */
+  G1:sublist(G,lambda([u],second(u)>tr)),
+  G2:map(lambda([u],[first(u),float(log(second(u)))]),G1),
   /* Compute the parameter estimation */
-  G4:apply('matrix,G3),
-  lsquares_estimates(G4,[t,y],y=g(t),[c,s,m])
+  G3:apply('matrix,G2),
+  lsquares_estimates(G3,[t,y],y=g(t),[c,s,m])
 )$
 
 /* ====== Functions to compute the Fittings =============== */
 
-getFittingFunctions(l,tr):=block([G,est],
+getFittingFunctions(l,tr):=block([G,est1,est2],
   /* tr is a lower threshold for data to be counted
      from the list of increments */ 
-  G:l[1],
-  est:FittingDelta(G,tr),
-  define(h1(t),subst(float(first(est)),h(t))),
-  G:l[2],
-  est:FittingDelta(G,tr),
-  define(h2(t),subst(float(first(est)),h(t))),
-  G:l[3],
-  est:FittingDelta(G,tr),
-  define(h3(t),subst(float(first(est)),h(t))),
-  [h1(t),h2(t),h3(t)])$
+  G:l[4],
+  est1:FitData(G,tr),
+  define(h1(t),subst(float(first(est1)),h(t))),
+  G:l[5],
+  est2:FitData(G,tr),
+  define(h2(t),subst(float(first(est2)),h(t))),
+  [est1,est2])$
 
 /* ====== Functions to plot the Results =============== */
 
-createPlot(l,max):=block([G1,G2,G3],
-  G1:l[1], G2:l[2], G3:l[3],
-  plot2d([[discrete, G1], h1(t), [discrete, G2], h2(t), [discrete, G3], h3(t)], 
+createPlot(l,max):=block([G1,G2],
+  G1:l[1], G2:l[2], 
+  plot2d([[discrete, G1], h1(t), [discrete, G2], h2(t)], 
   [t,0,200], [y,0,max],
   [style, points, lines], [legend, false],
-  [color, red, red, green, green, blue, blue])
+  [color, red, red, green, green])
 )$
 
 createDataPlot(l,max):=block([G1,G2,G3],
@@ -93,15 +90,16 @@ plotDoubling(h):=block([dh:diff(h,t)],
 /* ==== Italy, 60.48 Mio Einwohner ===== */
 
 l:getData(Italy);
-getFittingFunctions(l,50);
-createPlot(l,2*10^5);
-
-plotDoubling(h1(t));
-
+getFittingFunctions(l,2000);
+createPlot(l,4*10^5);
 
 /* ==== Spain, 46.66 Mio Einwohner ===== */
 
 l:getData(Spain);
+getFittingFunctions(l,4000);
+createPlot(l,4*10^5);
+est:FitData(l[4]); 
+
 getFittingFunctions(l,50);
 createPlot(l,2*10^5);
 
@@ -171,8 +169,6 @@ createPlot(l,2*10^4);
  347.3903236746284 (erf(0.1113184044913795 (t - 93.6891365970796)) + 1.0)]
 
 /* ############## Logistic curve ############### */ 
-
-l(t):=K/(1+exp(c-r*t));
 
 lFit(G,K0):=block([G1,M,est,m],
   G1:map(lambda([u,v],[u,log(K0/v-1)]),map(first,G),map(second,G)),
@@ -247,14 +243,23 @@ c/r = 88.41501116683081
 
 /* #### Dynamics of infected */ 
 
-plotInfected(l,max,c0,m0,s0):=block([G:l[4],f:subst([c=c0,s=s0,m=m0],f(t))],
+fitInfectedWithGauss(l,max,c0,m0,s0):=block(
+  [G:l[3],f:subst([c=c0,s=s0,m=m0],f(t))],
+  plot2d([[discrete, G], f],
+  [t,0,200], [y,0,max],
+  [style, points, lines], [legend, false],
+  [color, blue, red]))$
+
+fitInfectedWithLogistic(l,max,K0,c0,r0):=block(
+  [G:l[3],f:subst([c=c0,K=K0,r=r0],dl(t))],
   plot2d([[discrete, G], f],
   [t,0,200], [y,0,max],
   [style, points, lines], [legend, false],
   [color, blue, red]))$
 
 l:getData(Austria);
-plotInfected(l,10^4);
+fitInfectedWithGauss(l,10^4,9.14,93,12);
+fitInfectedWithLogistic(l,10^4,1.3*10^4,33,0.268);
 
 l:getData(China);
 plotInfected(l,10^4,4.66,-0.1,5);
